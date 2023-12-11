6b8e21243a90c7ba1f19a0d5c558a8d9ab9f76b0
Rowan Walker <rowan.walker@ginetta.net>
2023-12-11 14:18:41 +0100

    Revert "chore/infra: update go libs and deploy to vercel"
    This reverts commit f18174709229f2fb40a96fb4bfd94679e4a0e35f.

--- deploy/aws-lambda/main.go
+++ deploy/aws-lambda/main.go
@@ -23,7 +23,7 @@ func main() {
 	}
 
 	registry := prometheus.NewRegistry()
-	y := server.New(NewDynamo(os.Getenv("TABLE_NAME")), maxLength, registry, false, nil)
+	y := server.New(NewDynamo(os.Getenv("TABLE_NAME")), maxLength, registry, false)
 
 	algnhsa.ListenAndServe(
 		y.HTTPHandler(),
